526910ad412ab77de797dd7ac2f773fe20929cfd
Victor Sriqui <victorsriqui@gmail.com>
2023-02-08 17:40:12 -0700

    Add friend's list

--- friends.js
+++ friends.js
@@ -1,6 +1,11 @@
-var bestFriend = "Amir"
+var bestFriend = "mir"
+var otherFriend = "aze"
+var otherGoodFriend = "Miru"
 if (bestFriend === "Amir") {
     console.log(`Victor's best friend is ${bestFriend} `)
+    console.log(`Jason's best friend is ${otherFriend} `)
+}else if (bestFriend !== "Amir" && otherFriend !== "Naze")    {
+    console.log(`Friends are ${bestFriend}, ${otherFriend}, ${otherGoodFriend}`)
 } else {
-    console.log("This is not Victor's best friend.")
+    console.log("These are not our friends.")
 } 
